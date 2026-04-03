37da44e71116644d04239bec0aa506978cff8fc0
dararodrigxes <breezyado@gmail.com>
2026-04-02 22:07:48 -0300

    Modo Escuro

---
+++ js/interatividade.js
@@ -0,0 +1,43 @@
+$(function(){
+
+
+    //ENTRADA
+
+    var botoes = $(".botao");
+    var titulo = $("h1");
+    var subtitulo = $("#segundo");
+    var icones = $("svg")
+    var fundo = $("body")
+    var botao_modoEscuro = $("#modoEscuro");
+
+    var modoEscuro = false
+
+    //PROCESSAMENTO
+
+    botao_modoEscuro.click (()=>{
+
+          
+        if ( modoEscuro == false ){
+
+            botoes.css("background-color", "black");
+            botoes.css("color", "white");
+
+            modoEscuro = true;
+    
+        }else{
+
+            botoes.css("background-color", "white");
+            botoes.css("color", "black");
+
+            modoEscuro = false;
+
+        }
+
+
+    });
+
+    
+    //SAIDA
+
+
+});
