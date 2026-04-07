d2855c6548a1643ed2f836bb27c5791ffbecdae9
dararodrigxes <breezyado@gmail.com>
2026-04-07 20:19:50 -0300

    modoEscuro

--- js/interatividade.js
+++ js/interatividade.js
@@ -9,6 +9,8 @@ $(function(){
     var icones = $("svg")
     var fundo = $("body")
     var botao_modoEscuro = $("#modoEscuro");
+    var imgpersona = $("#persona");
+
 
     var modoEscuro = false
 
@@ -22,12 +24,28 @@ $(function(){
             botoes.css("background-color", "black");
             botoes.css("color", "white");
 
+            fundo.css("background-image", "url('https://i.ibb.co/93Bv2kMR/cdf72e93-1658-414c-989a-3689d414ca44.jpg')");
+
+            titulo.css("color", "white")
+            titulo.css("background-color" , "black")
+
+            imgpersona.css("border" , "15px solid rgba(255, 255, 255, 0.42)")
+
+        
+
             modoEscuro = true;
     
         }else{
 
             botoes.css("background-color", "white");
-            botoes.css("color", "black");
+            botoes.css("color", "brown");
+
+            fundo.css("background-image", "url('../recursos/img/fundo.jpg.jpg')");
+
+            titulo.css("color", "brown")
+            titulo.css("background-color" , "white")
+
+            imgpersona.css("border" , "15px solid rgba(70, 110, 4, 0.418)")
 
             modoEscuro = false;
 
